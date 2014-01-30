ea736e27e6211464050aef2454999081db0154b5
manu37 <manu37@f24e47d7-3ab8-45b6-a1fc-ab039324a57b>
2014-01-30 14:28:52 +0000

    hide CDN config options

--- trunk/tinymce4/config/metaconfig.php
+++ trunk/tinymce4/config/metaconfig.php
@@ -1,4 +1,6 @@
 <?php
 $plugin_mcf['tinymce4']['init']="function:tinymce_getInits";
 $plugin_mcf['tinymce4']['utf8_marker']="hidden";
+$plugin_mcf['tinymce4']['CDN_src']="hidden";
+$plugin_mcf['tinymce4']['CDN_host']="hidden";
 ?>
