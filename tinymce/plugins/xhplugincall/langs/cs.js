c9d43a61f4db4f2dbdfbcadecf9a9f4daab00207
manu37 <marinello@pixolution.ch>
2018-09-24 16:54:48 +0200

    minor fixes

--- tinymce/plugins/xhplugincall/langs/cs.js
+++ tinymce/plugins/xhplugincall/langs/cs.js
@@ -1,4 +1,4 @@
-tinymce.addI18n('de',{
+tinymce.addI18n('cs',{
 "Insert/edit a XH plugin call": "Vložit/upravit příkaz XH pluginu",
 "Insert/edit the XH plugin call here - without {{{}}}": "Zde vložit/upravit příkaz pluginu - bez {{{}}}"
 });
--- tinymce/plugins/xhplugincall/langs/sk.js
+++ tinymce/plugins/xhplugincall/langs/sk.js
@@ -1,4 +1,4 @@
-tinymce.addI18n('de',{
+tinymce.addI18n('sk',{
 "Insert/edit a XH plugin call": "Vložiť/upraviť XH plugin",
 "Insert/edit the XH plugin call here - without {{{}}}": "Tu vložiť/upraviť príkaz pluginu - bez {{{}}}"
 });
